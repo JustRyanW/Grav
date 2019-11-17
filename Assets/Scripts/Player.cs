101a0714f2443b71ef9a5d318837dae0e35cfa68
Ryan <xryanjw@gmail.com>
2019-11-17 13:44:00 +0000

    Added acutal project

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour {
+
+    Rigidbody2D thisRigidbody2D;
+    LineRenderer lineRenderer;
+
+    bool aiming;
+    bool falling;
+    Vector2 startForce;
+
+    public bool showTrajectory;
+    [Range(2,4000)]public int trajectorySegmentCount;
+
+    private void Start()
+    {
+        thisRigidbody2D = GetComponent<Rigidbody2D>();
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    private void Update()
+    {
+        startForce = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+
+        if (!falling)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                aiming = true;
+                lineRenderer.enabled = true;
+            } 
+            if (Input.GetMouseButtonUp(0))
+            {
+                falling = true;
+                aiming = false;
+                thisRigidbody2D.velocity = startForce;
+                thisRigidbody2D.constraints = RigidbodyConstraints2D.None;
+            }
+        }       
+    }
+
+    private void FixedUpdate()
+    {
+        if (aiming)
+        {
+            Calculations.Trajectory(thisRigidbody2D ,transform.position, startForce, ref lineRenderer, trajectorySegmentCount);
+        }
+        else if (falling)
+        {
+            thisRigidbody2D.AddForce(Calculations.Gravity(transform.position, thisRigidbody2D.mass) * thisRigidbody2D.mass);
+            Calculations.Trajectory(thisRigidbody2D ,transform.position, thisRigidbody2D.velocity, ref lineRenderer, trajectorySegmentCount);         
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        falling = false;
+        lineRenderer.enabled = false;
+        thisRigidbody2D.constraints = RigidbodyConstraints2D.FreezeAll;
+    }
+}
---
+++ Assets/Scripts/Voxels.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SquareData {
+
+    //  2----3----4 
+    //  |         | 
+    //  1         5 
+    //  |         | 
+    //  0----7----6 
+    public static readonly Vector2[] vertices = new Vector2[8]
+    {
+        new Vector2(0, 0),
+        new Vector2(0, 0.5f),
+        new Vector2(0, 1),
+        new Vector2(0.5f, 1),
+        new Vector2(1, 1),
+        new Vector2(1, 0.5f),
+        new Vector2(1, 0),
+        new Vector2(0.5f, 0)
+    };
+
+    public static readonly int[,] triangulations = new int[16, 12]
+    {
+        {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
+        {0, 1, 7, -1, -1, -1, -1, -1, -1, -1, -1, -1},
+        {2, 3, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
+        {0, 2, 3, 3, 7, 0, -1, -1, -1, -1, -1, -1},
+        { },
+        { },
+        { },
+        { },
+        { },
+        { },
+        { },
+        { },
+        { },
+        { },
+        { },
+        { }
+    };
+
+}
+
+public class Square {
+
+   
+    Vector3 position;
+}
+
+public class Voxels : MonoBehaviour {
+    
+}
