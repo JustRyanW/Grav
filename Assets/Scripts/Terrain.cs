5d60128efa5d1d435b4dc11dd9c5a713cd524a88
Ryan Walker <ryan.walker@tombola.com>
2019-12-16 17:55:48 +0000

    Update Terrain.cs

--- Assets/Scripts/Terrain.cs
+++ Assets/Scripts/Terrain.cs
@@ -2,10 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Terrian : MonoBehaviour {
-    
+public class Terrian : MonoBehaviour
+{
+
+    [Header("Settings")]
+    public Vector2Int chunkSize = new Vector2Int(8, 8);
+    public int renderDistance = 2;
+    public float surfaceValue = 0.5f;
+    public bool smoothed = true;
+    public Material material;
+
+    [Header("Private")]
+    List<Chunk> chunks = new List<Chunk>();
+    List<Chunk> updateQueue = new List<Chunk>();
+
+    private void Start() {
+        
+    }
+
+    private void Update() {
+        
+    }
+
+
 }
 
-public static class Chunk {
+public class Chunk
+{
+    float[,] voxelMap;
+    Vector3[,,] vertexMap;
+    int[,,] indexMap;
 
+    public List<Vector3> verts = new List<Vector3>();
+    public List<int> indices = new List<int>();
+
+    Mesh mesh;
+    MeshFilter meshFilter;
+
+    // methods
 }
